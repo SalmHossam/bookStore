b1e5ec84b7d5e838a79bdca53ba3c80c5804fd9f
Salma Hossam <88389052+SalmHossam@users.noreply.github.com>
2024-03-21 17:33:38 +0200

    Add files via upload

---
+++ book_store/src/src/entity/Book.java
@@ -0,0 +1,56 @@
+package entity;
+
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private double price;
+    private int quantity;
+
+    public Book(int id, String title, String author, String genre, double price, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
