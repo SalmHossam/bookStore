8fcfab8f3faa898e08f5b20875660d826ca96513
Salma Hossam <88389052+SalmHossam@users.noreply.github.com>
2024-03-22 12:52:56 +0200

    Update bookController.java

--- book_store/src/controller/bookController.java
+++ book_store/src/controller/bookController.java
@@ -10,9 +10,11 @@ public class bookController {
     private Connection connection;
 
     public bookController() {
-        String url= "jdbc:mysql://localhost/" + dbName + "?user=" + username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8";
-        String username = "root";
+         String username = "root";
         String password = "Salma@2001";
+        String dbName = "bookStore";
+        String url= "jdbc:mysql://localhost/" + dbName + "?user=" + username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8";
+      
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
