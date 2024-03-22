71c3c9c1bd94743fd4998631d105c0b69df6c07f
Salma Hossam <88389052+SalmHossam@users.noreply.github.com>
2024-03-22 21:37:53 +0200

    Update userController.java

--- book_store/src/controller/userController.java
+++ book_store/src/controller/userController.java
@@ -1,7 +1,8 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import entity.User;
+
+import java.sql.*;
 
 public class userController {
 
@@ -9,7 +10,7 @@ public class userController {
 
     public userController() {
         String username = "root";
-        String password = "Salma@2001";
+        String password = "";
         String dbName = "bookStore";
 
         String url = "jdbc:mysql://localhost/" + dbName + "?user=" + username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8";
@@ -22,6 +23,53 @@ public class userController {
             System.out.println(e);
         }
 
+
+    }
+    public void addUser(User user){
+        String sql="INSERT INTO users(name,username,password) VALUES(?,?,?)";
+        try(PreparedStatement statement=connection.prepareStatement(sql)){
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getUsername());
+            statement.setString(3,user.getPassword());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    public void deleteUser(int id){
+        String sql="DELETE FROM users WHERE id=?";
+        try(PreparedStatement statement=connection.prepareStatement(sql)){
+            statement.setInt(1,id);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void updatePassword(User user){
+        String sql="UPDATE users SET password=?";
+        try(PreparedStatement statement=connection.prepareStatement(sql)){
+            statement.setString(1,user.getPassword());
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public boolean getUser(User user){
+        String sql="SELECT FROM users WHERE username=? AND password=?";
+        User user1=null;
+        try(PreparedStatement statement=connection.prepareStatement(sql)){
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            ResultSet resultSet=statement.executeQuery();
+           return resultSet.next();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return false;
+        }
 
     }
 }
