bcea4f12304ecad78f637a40ee1396bcf07b9bf7
Salma Hossam <88389052+SalmHossam@users.noreply.github.com>
2024-03-22 12:52:18 +0200

    Update userController.java

--- book_store/src/controller/userController.java
+++ book_store/src/controller/userController.java
@@ -1,5 +1,27 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class userController {
 
+    private Connection connection;
+
+    public userController() {
+        String username = "root";
+        String password = "Salma@2001";
+        String dbName = "bookStore";
+
+        String url = "jdbc:mysql://localhost/" + dbName + "?user=" + username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8";
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
 }
